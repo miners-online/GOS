c0bce8abe399c063a3862c799b53130e324a921f
ajh123 <ajh123@users.noreply.github.com>
2024-05-12 15:18:34 +0100

    feat: add installer

---
+++ src/startup.lua
@@ -0,0 +1,34 @@
+currentVersion = 2.0
+local function downloadFiles(url, path)
+    for i = 1, 3 do
+        local response = http.get(url)
+        if response then
+            local data = response.readAll()
+            if path then
+                local f = io.open(path, "w")
+                f:write(data)
+                f:close()
+            end
+            return true
+        end
+    end
+    return false
+end
+
+function checkForUpdates(updateUrl)
+    for i = 1, 3 do
+        local response = http.get(updateUrl)
+        if response then
+            local data = response.readAll()
+            if data ~= currentVersion then
+                print("There's an update!!!")
+                sleep(1)
+                if not fs.exists("/tmp/installer") then
+                    downloadFiles("https://raw.githubusercontent.com/miners-online/GOS/main/installer.lua", "/tmp/installer")
+                end
+                shell.run("/tmp/installer")
+            end
+        end
+    end
+end
+checkForUpdates("https://raw.githubusercontent.com/miners-online/GOS/main/version")
